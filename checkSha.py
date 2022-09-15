276c8a463103c5a491b9ea51ffc42fa0ada296bc
Zhi Ma <mazh@oregonstate.edu>
2022-09-15 00:48:50 -0700

    added url parameter

--- checkSha.py
+++ checkSha.py
@@ -4,10 +4,11 @@ os.system('cmd /c "git log"')
 # git ls-remote https://github.com/zhiweim/HasamiShogi.git pulls the latest SHA from remote repository
 
 
-def checkSha():
+def checkSha(url):
     current_long = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
     current_short = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
     print(current_long)
     print(current_short)
+
 
 
