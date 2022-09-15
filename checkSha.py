89155d06a181bcba30f17676a58a6544d3bcd371
Zhiwei Ma <zhiweimaw@gmail.com>
2022-09-15 02:56:46 -0700

    updated checkSha

--- checkSha.py
+++ checkSha.py
@@ -1,14 +1,22 @@
-import os, subprocess
-
-os.system('cmd /c "git log"')
-# git ls-remote https://github.com/zhiweim/HasamiShogi.git pulls the latest SHA from remote repository
-
+import subprocess
 
 def checkSha(url):
-    current_long = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
-    current_short = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
-    print(current_long)
-    print(current_short)
+    """
+    Takes in the url of the remote repository and compares the local commit SHA to the latest
+    commit SHA on the remote repository
+
+    :param url: url for the remote github repository
+    :return: bool
+    """
+    local_long = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
+    local_short = local_long[0:7]
+    master_long = subprocess.check_output(('git', 'ls-remote', f"{url}")).decode('ascii').strip()[0:40]
+    master_short = master_long[0:7]
+
+    return local_long == master_long
+
+print(checkSha("https://github.com/zhiweim/HasamiShogi.git"))
+
 
 
 
