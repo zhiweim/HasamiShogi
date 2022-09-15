8a29d2c0599d3f443177ae2be8ca5060f5d40e24
Zhiwei Ma <zhiweimaw@gmail.com>
2022-09-15 00:40:33 -0700

    Added checkSha func

--- checkSha.py
+++ checkSha.py
@@ -1,3 +1,13 @@
-import os, argparse
+import os, subprocess
 
 os.system('cmd /c "git log"')
+# git ls-remote https://github.com/zhiweim/HasamiShogi.git pulls the latest SHA from remote repository
+
+
+def checkSha():
+    current_long = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
+    current_short = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
+    print(current_long)
+    print(current_short)
+
+
